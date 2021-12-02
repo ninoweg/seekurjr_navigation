a5d7867d71429d67875220f06727cad4045b8eca
Nino Wegleitner <94084069+ninoweg@users.noreply.github.com>
2021-12-02 11:48:53 +0100

    Update bumper_state_V2.cpp

--- src/bumper_state_V2.cpp
+++ src/bumper_state_V2.cpp
@@ -1,10 +1,6 @@
 #include <ros/ros.h>
-#include <move_base_msgs/MoveBaseAction.h>
-#include <actionlib/client/simple_action_client.h>
 #include <std_srvs/Empty.h>
 #include <rosaria/BumperState.h>
-
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 void bumperstateCallback(const rosaria::BumperState::ConstPtr& msg, bool &state)
 {
@@ -22,16 +18,7 @@ int main(int argc, char** argv){
   static std::vector<double> home;
   bool state;
 
-  ros::init(argc, argv, "simple_navigation_goals");
-  
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
-
-  //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-  ROS_INFO("move_base action server is up");
+  ros::init(argc, argv, "bumper_state");
 
   ros::NodeHandle n;
   ros::Subscriber bumper_state = n.subscribe<rosaria::BumperState>("/rosaria/bumper_state", 1000, boost::bind(bumperstateCallback, _1, state));
