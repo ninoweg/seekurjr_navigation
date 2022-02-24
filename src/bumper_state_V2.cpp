9e6bd2c62847bfa4ead9015b5d680a0200b1470c
Nino Wegleitner <94084069+ninoweg@users.noreply.github.com>
2022-02-24 23:13:25 +0100

    Update bumper_state_V2.cpp

--- src/bumper_state_V2.cpp
+++ src/bumper_state_V2.cpp
@@ -2,33 +2,33 @@
 #include <std_srvs/Empty.h>
 #include <rosaria/BumperState.h>
 
-void bumperstateCallback(rosaria::BumperState::ConstPtr msg)
-{
-  for(int i = 0; i<4; i++)
-  {
-    if(msg->front_bumpers.at(i)==1 || msg->rear_bumpers.at(i)==1)
-    {
-      ROS_INFO("Bumper was activated!");
-      ROS_INFO("Wait for RosAria disable_motors service ... ");
-      motor.waitForExistence();
-      ROS_INFO("Call RosAria disable_motors service ... ");
-      motor.call(srv);
-      ROS_INFO("Motors deactivated.");
-    }
-  }
-}
-
 class BumperDisableMotor 
 {
 private:
   ros::ServiceClient motor;
   ros::Subscriber bumper_state;
+  ros::ServiceClient enable_motor
   std_srvs::Empty srv;
 public:
   void BumperDisableMotor(ros::NodeHandle& n)
   {
     bumper_state = n.subscribe("/rosaria/bumper_state", 10, &bumperstateCallback, this);
-    motor = n.serviceClient<std_srvs::Empty>("/rosaria/disable_motors"); 
+    motor = n.serviceClient<std_srvs::Empty>("/rosaria/disable_motors");
+  }
+  void bumperstateCallback(rosaria::BumperState::ConstPtr msg)
+  {
+    for(int i = 0; i<4; i++)
+    {
+      if(msg->front_bumpers.at(i)==1 || msg->rear_bumpers.at(i)==1)
+      {
+        ROS_INFO("Bumper was activated!");
+        ROS_INFO("Wait for RosAria disable_motors service ... ");
+        motor.waitForExistence();
+        ROS_INFO("Call RosAria disable_motors service ... ");
+        motor.call(srv);
+        ROS_INFO("Motors deactivated.");
+      }
+    }
   }
 }
 
