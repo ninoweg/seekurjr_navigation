abcd57185ccf6a7aae0e6fe7a66054a442eba22a
Nino Wegleitner <94084069+ninoweg@users.noreply.github.com>
2022-02-24 18:43:06 +0100

    Update bumper_state_V2.cpp

--- src/bumper_state_V2.cpp
+++ src/bumper_state_V2.cpp
@@ -2,45 +2,40 @@
 #include <std_srvs/Empty.h>
 #include <rosaria/BumperState.h>
 
-int state = 0;
-
-void bumperstateCallback(const rosaria::BumperState::ConstPtr& msg)
+void bumperstateCallback(rosaria::BumperState::ConstPtr msg)
 {
-  for(int k = 0; k<4; k++)
+  for(int i = 0; i<4; i++)
   {
-    if(msg->front_bumpers.at(k)==1 || msg->rear_bumpers.at(k)==1)
+    if(msg->front_bumpers.at(i)==1 || msg->rear_bumpers.at(i)==1)
     {
-      ROS_INFO("Bumper activated!");
-      state = 1;
-    }
-  }
-}
-
-int main(int argc, char** argv){
-  static std::vector<double> goals;
-  static std::vector<double> home;
-  
-  ros::init(argc, argv, "bumpers_disable_motor");
-
-  ros::NodeHandle n;
-  // ros::Subscriber bumper_state = n.subscribe<rosaria::BumperState>("/rosaria/bumper_state", 1000, boost::bind(bumperstateCallback, _1, state));
-  ros::Subscriber bumper_state = n.subscribe("/rosaria/bumper_state", 1000, bumperstateCallback);
-  ros::ServiceClient motor = n.serviceClient<std_srvs::Empty>("/rosaria/disable_motors"); 
-  std_srvs::Empty srv;
-
-  while(true)
-  {
-    if(state==1)
-    {
+      ROS_INFO("Bumper was activated!");
       ROS_INFO("Wait for RosAria disable_motors service ... ");
       motor.waitForExistence();
       ROS_INFO("Call RosAria disable_motors service ... ");
       motor.call(srv);
-      ROS_INFO("Bumper activated! Motors deactivated.");
-      state = 0;
+      ROS_INFO("Motors deactivated.");
     }
-    ros::spinOnce();
   }
+}
 
+class BumperDisableMotor 
+{
+private:
+  ros::ServiceClient motor;
+  ros::Subscriber bumper_state;
+  std_srvs::Empty srv;
+public:
+  void BumperDisableMotor(ros::NodeHandle& n)
+  {
+    bumper_state = n.subscribe("/rosaria/bumper_state", 10, &bumperstateCallback, this);
+    motor = n.serviceClient<std_srvs::Empty>("/rosaria/disable_motors"); 
+  }
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "bumpers_disable_motor");
+  ros::NodeHandle n;
+  BumperDisableMotor bdm = BumperDisableMotor(n);
+  ros::Spin();
   return 0;
 }
